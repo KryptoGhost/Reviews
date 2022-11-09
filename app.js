bc9b460306482a3c739f9d7beb3ee8756886b9bf
KryptoGhost <106913123+KryptoGhost@users.noreply.github.com>
2022-11-09 21:46:14 +0100

    another change

--- app.js
+++ app.js
@@ -37,14 +37,14 @@ const reviews = [
     },
   ];
 
-  const img = document.getElementById("person-img");
+  const img = document.getElementById("img-person");
   const author = document.getElementById("author");
   const job = document.getElementById("job");
   const info = document.getElementById("info");
 
   const prevBtn = document.querySelector(".prev-btn");
   const nextBtn = document.querySelector(".next-btn");
-  const randomBtn = document.querySelector(".random-btn");
+  const randomBtn = document.querySelector("#random-btn");
 
   let currentItem = 0;
 
